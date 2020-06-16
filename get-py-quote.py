73d26a0a953f50609817cb2a3cded8693a97f5ab
krishna <bkesari@outlook.com>
2020-06-16 10:16:56 -0400

    Random Array

--- get-py-quote.py
+++ get-py-quote.py
@@ -1,11 +1,14 @@
 def main():
   # print("Keep it logically awesome.")
+  import random
+  last = 13
+  rnd = random.randint(0, last)
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[-1])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
